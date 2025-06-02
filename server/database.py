4b6806761111a7074caa4758ebb21840f56b7193
REBOSOLUTIONS <siva.prasad@rebosolution.com>
2025-06-02 12:42:17 +0530

    connect to new db of  RllFx

--- server/database.py
+++ server/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # DATABASE_LOCAL_URL = "postgresql://postgres:1q2w3e4r@localhost:5432/crypto"
-DATABASE_REMOTE_URL = "postgresql://xfx2025:OAg0tocVgxBkg98JDWA4EUp60M4D48r4@dpg-d0j2pladbo4c73bvhjhg-a.oregon-postgres.render.com/crypto_l5ku"
+DATABASE_REMOTE_URL = "postgresql://mycryptodb_user:b8TJYjiDyYoPXKUcav3PuhNe7OGmXyfr@dpg-d0ukr7umcj7s739rbuo0-a.oregon-postgres.render.com/mycryptodb"
 
 # engine = create_engine(DATABASE_LOCAL_URL)
 engine = create_engine(DATABASE_REMOTE_URL)
